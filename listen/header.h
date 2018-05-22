88fe6aadd052f52a0abefdfa9ed7c777d187bab1
Franz <inf17052@lehre.dhbw-stuttgart.de>
2018-05-23 00:14:45 +0200

    listen now does anything you wish of a class A programm

--- listen/header.h
+++ listen/header.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define STRINGLENGTH 31
 
@@ -11,9 +12,13 @@ typedef struct linList{
 typedef struct listField{
 	struct linList* start;
 	struct linList* momentan;
-}
+}linListField;
 
 linList_p linListAllocCell(char* c);
 void linListFreeCell(linList_p junk);
-void linListFree(linList_p *anchor)
-
+void linListFree(linListField* f);
+void linListInsertFirst(linListField* f, linList_p newFirstCell);
+void linListInsertLast(linListField* f, linList_p newLasttCell);
+linList_p linListExtractFirst(linListField* f);
+void linListPrint(linListField f);
+void linListPrintReverse(linListField f);
--- listen/main.c
+++ listen/main.c
@@ -3,31 +3,121 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	listField l_field;
+	linListField l_field;
+	l_field.start = 0;
+	l_field.momentan = 0;
 	
-	linList_p newPoin = linListAllocCell("abcdefg");
+	int amount;
+	int counter = 0;
+	char stringBuffer[255+1];
+	printf("wie viele strings sollen eingelesen werden?\n");
+	scanf("%d",&amount);
+	fflush(stdin);
 	
-	printf("mein char ist: %s",newPoin->text);	
 	
+	while(counter<amount){
+		scanf("%255s",stringBuffer);
+		fflush(stdin);
+		if(strlen(stringBuffer)<256){
+			linList_p newElement = linListAllocCell(stringBuffer);
+			linListInsertLast(&l_field,newElement);
+			counter++;
+		}
+	}
+	
+	linListPrint(l_field);
+	linListPrintReverse(l_field);
+	
+/*	linList_p newPoin = linListAllocCell("abcdefg");	
+	linList_p newPoin2 = linListAllocCell("petrusmeinsohn");	
+		
+	
+	linListInsertFirst(&l_field,newPoin);
+	linListInsertFirst(&l_field,newPoin2);
+	linListPrint(l_field);
+	
+	linListFree(&l_field);
+	linListPrint(l_field);
 	return 0;
-}
+*/}
 
 linList_p linListAllocCell(char* payload){
 	linList_p retCell = (linList_p) malloc(sizeof(linListCell));
 	char* myString = (char*) malloc(STRINGLENGTH);
 	strcpy(retCell->text,payload);
+	retCell->next = 0;
 	return retCell;
 }
 
-void linListFreeCell(linList_p junk){
+void linListFreeCell(linList_p junk){   //noch nicht fertig. schauen ob die cell am anfang ist
 	free(junk);
 }
 
-void linListFree(listField){
-	linList_p listEl;
-	while(listEl->next){
-		listEl = listEl->next;
-		free(listEl)
+void linListFree(linListField* f){
+	f->momentan = f->start;
+	while(f->momentan!=0){
+		f->momentan = f->start->next;
+		linListFreeCell(f->start);
+		f->start = f->momentan;
 	}
+	printf("everything has been deleted my friend\n");
+}
+
+void linListInsertFirst(linListField* f, linList_p newFirstCell){   //nicht sicher, warum das einen rückgabewert braucht
+	newFirstCell->next = f->start;
+	f->start = newFirstCell;
+	printf("%s als erstes Element eingefügt\n",f->start->text);
+}
+
+void linListInsertLast(linListField* f, linList_p newLastCell){   //nicht sicher, warum das einen rückgabewert braucht4
+	if(f->start==0){
+		f->start = newLastCell;
+		f->momentan = f->start;
+	}else{
+		f->momentan = f->start;
+		while(f->momentan->next){
+			f->momentan = f->momentan->next;
+		}
+		f->momentan->next = newLastCell;
+		f->momentan = f->momentan->next;
+	}
+	printf("%s als letztes Element eingefügt\n",f->momentan->text);
+}
+
+linList_p linListExtractFirst(linListField* f){
+	return f->start;
+}
+
+void linListPrint(linListField f){
+	printf("=======================================\n");
+	printf(">list from first to last\n");
+	f.momentan = f.start;
+	while(f.momentan){
+		printf("%s\n",f.momentan->text);
+		f.momentan=f.momentan->next;
+	}
+}
+
+void linListPrintReverse(linListField f){
+	linList_p elemBuffer;
+	printf("=======================================\n");
+	printf(">list from last to first\n");
+	f.momentan = f.start;
+	while(f.momentan){
+		f.momentan=f.momentan->next;
+	}
+	//f.momentan now on last element
+	elemBuffer = f.momentan;
+	f.momentan = f.start;
+	
+	while(elemBuffer!=f.start){
+		while(f.momentan->next!=elemBuffer){
+			f.momentan =f.momentan->next;
+		}
+		printf("%s\n",f.momentan->text);
+		elemBuffer = f.momentan;
+		f.momentan=f.start;
+	}
+	
 	
 }
